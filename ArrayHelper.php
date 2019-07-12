f83f7c0e126ff4d1708b418b602ca28eba3efc13
wangxin <wangxin.king@foxmail.com>
2019-07-12 10:16:05 +0800

    add a method

--- ArrayHelper.php
+++ ArrayHelper.php
@@ -14,8 +14,8 @@ class ArrayHelper
     /**
      * 获取除指定键数组外的所有给定数组。
      *
-     * @param array $array
-     * @param array $keys
+     * @param array $array 原数组
+     * @param array $keys 要舍去的key
      *
      * @return array
      * @author King
@@ -25,5 +25,39 @@ class ArrayHelper
         return array_diff_key($array, array_flip($keys));
     }
 
+    /**
+     * 二分查找
+     *
+     * @param int|float $number 查找数
+     * @param array     $array 待查找区间
+     *
+     * @return int
+     * @author King
+     */
+    public static function binarySearch($number, array $array)
+    {
+        if (!is_array($array) || empty($array)) {
+            return -1;
+        }
+
+        sort($array);
+
+        $length = count($array);
+        $lower  = 0;
+        $high   = $length - 1;
+
+        while ($lower < $high) {
+            $middle = intval(($lower + $high) / 2);
+            if ($array[$middle] > $number) {
+                $high = $middle - 1;
+            } elseif ($array[$middle] < $number) {
+                $lower = $middle + 1;
+            } else {
+                return $middle;
+            }
+        }
+
+        return -1;
+    }
 
 }
