f45604803b6905bcfc985b9d29797e2f1a9fc399
wangxin <wangxin.king@foxmail.com>
2019-07-13 18:26:09 +0800

    helper test

---
+++ tests/ArrayHelpTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: King
+ * Date: 2019/07/13
+ * Time: 17:55
+ */
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use wangxin\helper\ArrayHelper;
+
+class ArrayHelpTest extends TestCase
+{
+    /**
+     * testExcept
+     * @author King
+     */
+    public function testExcept()
+    {
+        $data   = ['a' => '123', 'b' => '456', 'c' => '789'];
+        $except = ['a', 'c'];
+
+        $arr = ArrayHelper::except($data, $except);
+
+        $this->assertIsArray($arr);
+        $this->assertArrayHasKey('b', $arr);
+        $this->assertArrayNotHasKey('a', $arr);
+        $this->assertArrayNotHasKey('c', $arr);
+    }
+}
